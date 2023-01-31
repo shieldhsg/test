04213121aab5ababdd60f47135bf2dadf2078e1c
hushengguo <hushengguo@xiaomi.com>
2023-01-31 15:01:05 +0800

    u

--- main.go
+++ main.go
@@ -3,19 +3,7 @@ package main
 import "fmt"
 
 func main(){
-	// do modify
-
 	fmt.Println("start test")
 
-	// do some modify2
-
-	// do some modify3
-
-	// do more modify
-
-	// do more more
-
-	// do more 3
-
-	// do more 4
+	// do merge op
 }
